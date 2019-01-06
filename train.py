f909352078654e221d4f7980fc177ffa18c8c64f
eifuentes <efuentes405@gmail.com>
2019-01-06 15:33:16 -0800

    add support for tensorboard and floydhub metrics

--- train.py
+++ train.py
@@ -1,3 +1,5 @@
+import json
+import os
 import random
 from pathlib import Path
 from random import randint
@@ -8,11 +10,10 @@ import PIL
 import torch
 import torch.nn.functional as F
 import torchvision.transforms as vtransforms
-from ignite.contrib.handlers import ProgressBar
 from ignite.contrib.metrics import ROC_AUC
 from ignite.engine import (Events, create_supervised_evaluator,
                            create_supervised_trainer)
-from ignite.metrics import BinaryAccuracy, Loss, RunningAverage
+from ignite.metrics import BinaryAccuracy, Loss
 from torch.optim import Adam
 from torch.utils.data import DataLoader, random_split
 
@@ -20,9 +21,40 @@ from dataset import PCamDataset, calculate_statistics
 from model import WideResNetBinaryClassifier
 
 
-def run(data_dir, validation_size=0.10, batch_size=64,
+def create_summary_writer(model, dloader, logdir):
+    try:
+        from tensorboardX import SummaryWriter
+    except ImportError:
+        print('failed to import tensorboardX')
+        return None
+    writer = SummaryWriter(log_dir=logdir)
+    x, y = next(iter(dloader))
+    try:
+        writer.add_graph(model, x)
+    except Exception as e:
+        print(f'failed to save model graph: {e}')
+    return writer
+
+
+def fmt_metric(mode, name, value, step):
+    metric = {
+        'metric': str(mode) + '_' + str(name),
+        'value': f'{value:.5f}',
+        'step': f'{int(step)}'
+    }
+    return json.dumps(metric)
+
+
+def log_metric(mode, name, value, step, writer=None):
+    print(fmt_metric(mode, name, value, step))
+    if writer:
+        writer.add_scalar(f'{mode}/{name}', value, step)
+
+
+def run(datadir, outdir, validation_size=0.10, batch_size=64,
         max_epochs=10, lr=1e-3, beta1=0.9, beta2=0.999,
-        num_workers=8, seed=None):
+        num_workers=8, seed=None,
+        log_iter_interval=10, logdir=None):
     # setup and device specific config
     seed = seed if seed else randint(1, 1000)
     random.seed(seed)
@@ -40,7 +72,7 @@ def run(data_dir, validation_size=0.10, batch_size=64,
         dloader_args = {'num_workers': num_workers, 'pin_memory': False}
 
     # load training labels and calculate class info
-    label_filename = Path(data_dir, 'train_labels.csv')
+    label_filename = Path(datadir, 'train_labels.csv')
     label_df = pd.read_csv(label_filename)
     num_samples = label_df['label'].shape[0]
     label_cnts = label_df['label'].value_counts()
@@ -49,14 +81,14 @@ def run(data_dir, validation_size=0.10, batch_size=64,
     print(f'Number of Samples: {num_samples}')
 
     # create datasets, verify integrity of dataset, and calculate statistics
-    training_dset = PCamDataset(data_dir, 'train',
+    training_dset = PCamDataset(datadir, 'train',
                                 label_filename,
                                 transform=vtransforms.ToTensor())
     num_val_samples = int(np.floor(len(training_dset) * validation_size))
     num_train_samples = len(training_dset) - num_val_samples
     train_dset, val_dset = random_split(training_dset,
                                         [num_train_samples, num_val_samples])
-    test_dset = PCamDataset(data_dir, 'test', transform=vtransforms.ToTensor())
+    test_dset = PCamDataset(datadir, 'test', transform=vtransforms.ToTensor())
     train_mean, train_std = calculate_statistics(train_dset,
                                                  batch_size, num_workers)
     val_mean, val_std = calculate_statistics(val_dset,
@@ -91,11 +123,12 @@ def run(data_dir, validation_size=0.10, batch_size=64,
     # test_dloader = DataLoader(test_dset, batch_size=batch_size,
     #                           shuffle=False, **dloader_args)
 
-    # setup model, optimizer, trainers and evaluators
+    # setup model, optimizer, tensorboard writer, trainers and evaluators
     model = WideResNetBinaryClassifier(in_channels=3,
                                        num_groups=3, num_blocks_per_group=3,
                                        channel_scale_factor=6, init_num_channels=16,
                                        dropout_proba=0.0, residual_scale_factor=0.2)
+    writer = create_summary_writer(model, train_dloader, logdir)
     optimizer = Adam(model.parameters(), lr=lr, betas=(beta1, beta2))
     trainer = create_supervised_trainer(model, optimizer,
                                         F.binary_cross_entropy,
@@ -104,56 +137,50 @@ def run(data_dir, validation_size=0.10, batch_size=64,
     eval_metrics = {
         'accuracy': BinaryAccuracy(),
         'bce': Loss(F.binary_cross_entropy),
-        'roc': ROC_AUC(F.binary_cross_entropy)
+        'roc': ROC_AUC()
     }
     evaluator = create_supervised_evaluator(model,
                                             metrics=eval_metrics,
                                             device=device,
                                             non_blocking=non_blocking)
 
-    # attach handlers
-    RunningAverage(output_transform=lambda x: x).attach(trainer, 'loss')
-
-    pbar = ProgressBar(persist=True)
-    pbar.attach(trainer, ['loss'])
+    # attach handlers for stdout and tensorboard
+    @trainer.on(Events.ITERATION_COMPLETED)
+    def log_training_loss(engine):
+        niter = (engine.state.iteration - 1) % len(train_dloader) + 1
+        if niter % log_iter_interval == 0:
+            log_metric('training', 'loss', engine.state.output, engine.state.iteration, writer)
 
     @trainer.on(Events.EPOCH_COMPLETED)
     def log_training_results(engine):
         evaluator.run(train_dloader)
         metrics = evaluator.state.metrics
-        avg_accuracy = metrics['accuracy']
-        avg_bce = metrics['bce']
-        avg_roc = metrics['roc']
-        pbar.log_message(
-            f'Training - Epoch: {engine.state.epoch} \
-              ROC: {avg_roc:.2f} \
-              Accuracy: {avg_accuracy:.2f} \
-              Loss: {avg_bce:.2f}'
-        )
+        avg_accuracy, avg_bce, avg_roc = metrics['accuracy'], metrics['bce'],  metrics['roc']
+        log_metric('training', 'bce', avg_bce, engine.state.iteration, writer)
+        log_metric('training', 'roc', avg_roc, engine.state.iteration, writer)
+        log_metric('training', 'accuracy', avg_accuracy, engine.state.iteration, writer)
 
     @trainer.on(Events.EPOCH_COMPLETED)
     def log_validation_results(engine):
         evaluator.run(val_dloader)
         metrics = evaluator.state.metrics
-        avg_accuracy = metrics['accuracy']
-        avg_bce = metrics['bce']
-        avg_roc = metrics['roc']
-        pbar.log_message(
-            f'Validation - Epoch: {engine.state.epoch} \
-              ROC: {avg_roc:.2f} \
-              Accuracy: {avg_accuracy:.2f} \
-              Loss: {avg_bce:.2f}'
-        )
-        pbar.n = pbar.last_print_n = 0
+        avg_accuracy, avg_bce, avg_roc = metrics['accuracy'], metrics['bce'],  metrics['roc']
+        log_metric('validation', 'bce', avg_bce, engine.state.iteration, writer)
+        log_metric('validation', 'roc', avg_roc, engine.state.iteration, writer)
+        log_metric('validation', 'accuracy', avg_accuracy, engine.state.iteration, writer)
 
     # conduct training
     trainer.run(train_dloader, max_epochs=max_epochs)
+
+    # save model and its assets
+    torch.save(model.state_dict(), os.path.join(outdir, 'model'))
 
 
 if __name__ == "__main__":
     import argparse
     parser = argparse.ArgumentParser(description='PCam Training Script')
     parser.add_argument('--datadir', type=str, default='/input/')
+    parser.add_argument('--outdir', type=str, default='assets')
     parser.add_argument('--val-size', type=float, default=0.10)
     parser.add_argument('--batch-size', type=int, default=64)
     parser.add_argument('--max-epochs', type=int, default=10)
@@ -162,8 +189,14 @@ if __name__ == "__main__":
     parser.add_argument('--beta2', type=float, default=0.999)
     parser.add_argument('--num-workers', type=int, default=8)
     parser.add_argument('--seed', type=int, default=None)
+    parser.add_argument('--log-iter-interval', type=int, default=10)
+    parser.add_argument('--logdir', type=str, default='logs')
     args = parser.parse_args()
 
-    run(args.datadir, args.val_size, args.batch_size,
+    os.makedirs(args.outdir, exist_ok=True)
+    os.makedirs(args.logdir, exist_ok=True)
+
+    run(args.datadir, args.outdir, args.val_size, args.batch_size,
         args.max_epochs, args.lr, args.beta1, args.beta2,
-        args.num_workers, args.seed)
+        args.num_workers, args.seed,
+        args.log_iter_interval, args.logdir)
